893606464ea4db954369279a3bda602da489dc9f
Bibhuti5501 <bibhutisingh8765@gmail.com>
2025-03-02 23:03:21 +0530

    Update Footer social media links to personal profiles

--- Layout/Footer.tsx
+++ Layout/Footer.tsx
@@ -4,18 +4,18 @@ import { SiGithub, SiLinkedin, SiFacebook, SiInstagram } from 'react-icons/si';
 function Footer() {
   return (
     <footer className="mt-5 p-10 flex justify-center">
-      <a className="mx-2" href="http://github.com/danmesfin">
+      <a className="mx-2" href="https://github.com/Bibhuti5501">
         <SiGithub />
       </a>
-      <a className="mx-2" href="http://linkedin.com/danielmesfin">
+      <a className="mx-2" href="https://www.linkedin.com/in/bibhutibhushansingh/">
         <SiLinkedin />
       </a>
-      <a className="mx-2" href="http://instagram.com/danmesfinn">
+      <a className="mx-2" href="https://www.instagram.com/bibhuti5501/">
         <SiInstagram />
       </a>
-      <a className="mx-2" href="http://facebook.com/">
+      {/* <a className="mx-2" href="https://x.com/BIBHUTI8765">
         <SiFacebook />
-      </a>
+      </a> */}
     </footer>
   );
 }
